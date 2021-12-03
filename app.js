8282106a39ee3e7a2e0e66eb11706cf30279f38c
Lyz03 <lizoe@netcourrier.com>
2021-12-03 11:40:34 +0100

    random selection

--- app.js
+++ app.js
@@ -1,6 +1,39 @@
 const waste = document.getElementById('waste');
 const trashes = document.querySelectorAll('.trash span');
 
+const yellow = [
+    "Bouteille plastique",
+    "Flacons plastiques",
+    "Boîtes en carton",
+    "Boîtes de conserve en fer",
+    "Barquettes en aluminium",
+    "Flacons plastiques"
+];
+
+const green = [
+    "bouteilles de vin",
+    "Bouteilles en verre",
+    "Pots de confiture",
+    "Bocaux de conserve"
+];
+
+const blue = [
+    "Journaux",
+    "Magazines"
+];
+
+const brown = [
+    "Restes de repas",
+    "Pots de yaourts"
+];
+
+let currentWaste = [];
+let randomSelection = [];
+let temporaryYellow;
+let temporaryGreen;
+let temporaryBlue;
+let temporaryBrown;
+
 // Drag and drop event
 waste.addEventListener('dragstart', dragStart);
 waste.addEventListener("dragend", dragEnd);
@@ -10,7 +43,7 @@ for (const index of trashes) {
     index.addEventListener('drop', drop);
 }
 
-// drag and drop function
+// drag and drop functions
 function dragOver(e) {
     e.preventDefault();
 }
@@ -29,3 +62,32 @@ function drop(e) {
 }
 
 // quelle couleur tenter e.path[1] === span#yellow
+
+// create a random index
+function randomNumber(array) {
+    return Math.floor(Math.random() * array.length);
+}
+
+// create the random selection of wastes
+function selection() {
+    temporaryYellow = yellow;
+    temporaryYellow.splice(randomNumber(yellow), 1);
+
+    temporaryGreen = green;
+    temporaryGreen.splice(randomNumber(green), 1);
+
+    temporaryBlue = [blue[randomNumber(blue)]];
+    temporaryBrown = [brown[randomNumber(brown)]];
+
+    return temporaryYellow.concat(temporaryGreen, temporaryBlue, temporaryBrown)
+}
+
+//shuffle array
+function shuffleArray(array){
+    return array.sort(()=> Math.random() - 0.5);
+}
+
+randomSelection = selection();
+randomSelection = shuffleArray(randomSelection);
+
+console.log(randomSelection)
